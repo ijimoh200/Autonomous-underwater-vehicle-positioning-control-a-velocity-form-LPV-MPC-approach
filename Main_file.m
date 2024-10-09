7ed506fed295379b63b24573560060d0e5e267a0
Isah Abdulrasheed Jimoh <79370909+ijimoh200@users.noreply.github.com>
2024-10-09 16:41:04 +0100

    Add files via upload

---
+++ Main_file.m
@@ -0,0 +1,127 @@
+%% POSITIONING CONTROL DURING DOCKING BASED ON LPV-MPC1
+
+% Initial values of the state vector
+x   = 0;            y       = 0;          z     = 0;
+phi = 0;            theta   = 0;          psi   = 0;
+u   = 0;            v       = 0;          w     = 0;
+p   = 0;            q       = 0;          r     = 0;
+eta_ini = [x y z phi theta psi]';
+nv_ini  = [u v w p q r]'; % Note nv is used to denote \nu
+
+% Sampling and simulation parameters
+Ts      = 0.1;
+Tf      = 30;
+NoS     = round(Tf/Ts);
+
+%% Generate reference trajectory and noise signals
+noise   = zeros(12,NoS);    % Measurement noise
+yref    = zeros(6,NoS);     % Reference signal
+nu_c = zeros(6,NoS);        % Unknown time-varying current disturbance
+for k = 1:NoS
+    % Reference signal:
+    if k <= round(NoS/4)
+        yref(:,k) = [0; 0; 0; 0; 0; 0];
+    else
+        yref(:,k) = [0.5; 0.5; 0.0; 0; 0; -0.1];
+    end
+    % Generate time-varying ocean current
+    if k <= round(NoS/2)
+        nu_c(:,k)  = [0; 0; 0; 0; 0; 0];
+    else
+        nu_c(:,k)  = [0.2*1; 0.2*1; 0.1; 0; 0; 0] ...
+            +[0.1*cos(3/20*pi*k*Ts)*cos(0.002*k*Ts); 0.1*sin(3/20*pi*k*Ts); 0.1*sin(0.002*k*Ts)*cos(3/20*pi*k*Ts); 0; 0; 0];
+    end
+end
+
+%% Simulate Controller
+%Proposed LPV-MPC1 in the conference paper:
+%Jimoh, I.A., Yue, H. and Küçükdemiral, I.B., 2023. Autonomous underwater
+% vehicle positioning control-a velocity form LPV-MPC approach.
+% IFAC-PapersOnLine, 56(2), pp.4388-4393.
+[tau1, x1, y1] = proposed(eta_ini,nv_ini,yref,nu_c,Ts,Tf);
+
+%% Plot Results
+t = 0:Ts:Tf-Ts;
+figure
+subplot(621)
+stairs(t, yref(1, :),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(1, 1:size(t,2)),'-r', 'linewidth', 1.2)
+ylabel('$x$ [m]', 'interpreter','latex')
+ylim([-0.1 0.6])
+grid on
+
+subplot(623)
+stairs(t, yref(2, 1:size(t,2)),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(2, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('$y$ [m]', 'interpreter','latex')
+ylim([-0.1 0.6])
+grid on
+
+subplot(625)
+stairs(t, yref(3, 1:size(t,2)),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(3, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylim([-0.05 0.03])
+ylabel('$z$ [m]', 'interpreter','latex')
+grid on
+
+subplot(627)
+stairs(t, yref(4, 1:size(t,2)),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(4, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('$\phi$ [rad]', 'interpreter','latex')
+ylim([-0.04 0.02])
+grid on
+
+subplot(629)
+stairs(t, yref(5, 1:size(t,2)),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(5, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('$\theta$ [rad]', 'interpreter','latex')
+grid on
+
+subplot(6,2,11)
+stairs(t, yref(6, 1:size(t,2)),'-.k', 'linewidth', 1.2)
+hold on
+stairs(t, y1(6, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('$\psi$ [rad]', 'interpreter','latex')
+legend('Target','LPCMPC1')
+xlabel ('Time [s]', 'interpreter','latex')
+ylim([-0.15 0.03])
+grid on
+
+subplot(622)
+stairs(t, tau1(1, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('X [N]', 'interpreter','latex')
+grid on
+
+subplot(624)
+stairs(t, tau1(2, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('Y [N]', 'interpreter','latex')
+grid on
+
+subplot(626)
+stairs(t, tau1(3, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('Z [N]', 'interpreter','latex')
+grid on
+
+subplot(628)
+stairs(t, tau1(4, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('K [Nm]', 'interpreter','latex')
+grid on
+
+
+subplot(6,2,10)
+stairs(t, tau1(5, 1:size(t,2)),'r', 'linewidth', 1.2)
+ylabel('M [Nm]', 'interpreter','latex')
+grid on
+
+subplot(6,2,12)
+stairs(t, tau1(6, 1:size(t,2)),'r', 'linewidth', 1.2)
+xlabel ('Time [s]', 'interpreter','latex')
+ylabel('N [Nm]', 'interpreter','latex')
+grid on
+
+
---
+++ proposed.m
@@ -0,0 +1,100 @@
+function [ui, xi, yi] = proposed(eta_ini,nv_ini,yref,nu_c,Ts,Tf)
+% This function implements the velocity form LPV-MPC1 cconsidering ocean
+% currents and measurement noise.
+% Cite: Jimoh, I.A., Yue, H. and Küçükdemiral, I.B., 2023. Autonomous underwater
+% vehicle positioning control-a velocity form LPV-MPC approach.
+% IFAC-PapersOnLine, 56(2), pp.4388-4393.
+
+% Simulation and MPC parameters
+NoS = round(Tf/Ts);
+nx = 6;                     % number of velocity variables
+nu = 6;                     % number of position variables
+ny = 6;                     % number of position (output) variables
+Q = blkdiag(1, 1, 1, 1, 1, 1)*1000;
+R = blkdiag(1, 1, 1, 1,1, 1)*0.002;
+N = 15;
+Nu = 2;
+
+% Memory allocations
+x        = zeros(nx,NoS);   % System states defined as the AUV velocities
+x(:,1)   = nv_ini;
+y        = zeros(ny,NoS);   % System output defined as the AUV position
+y(:,1)   = eta_ini;
+tau      = zeros(nu,NoS);   % Input foeces and mements
+Dtau     = zeros(nu,NoS);
+tau_     = zeros(nu, 1);    % Control input at time step k-1
+xtilde = zeros(nx+ny,NoS);  % Augemented state
+Dx        = zeros(nx,NoS);  % State increment Dx = x(k) - x(k-1)
+Dy        = zeros(ny,NoS);  % Output increment Dy = y(k) - y(k-1)
+tau_wave = 0;               % Defined as zero since ocean wave disturbances
+                            % are not considered
+
+% Main simulation loop
+for k = 1:NoS-1
+    yref_p = kron(ones(N,1), yref(:,k));    % Trajectory prediction
+
+    % Obtain update AUV matrices based on current state
+    [~,M, Jk, Ck, Dk,~] = Naminow_AUV([y(:,k);x(:,k)],tau_,0*nu_c(:,k),tau_wave);
+    Ak = eye(6)-inv(M)*(Ck+Dk)*Ts;
+    Bk =  inv(M)*Ts;
+    Hk = Jk*Ts;
+    Atilde = [Ak zeros(nx,ny); Hk eye(ny)];
+    Btilde = [Bk; zeros(ny, nu)];
+    Gtilde = [zeros(nx) eye(ny)];
+    Bdtilde = [zeros(nx); eye(nx)];
+    [X,~,~,~] = dare(Ak,Bk, Q,R,[],[]); % Computes terminal weight based...
+    % on discrete-time algebraic Riccati Eqn
+    S  = 1*X; % This may also be taken as Q
+
+    % Contruction of prediction model matrices
+    A_p = predA(Gtilde,Atilde,N);
+    B_p = predB(Gtilde,Atilde, Btilde, N, Nu);
+    Bd_p = predB(Gtilde,Atilde, Bdtilde, N, N);
+    Q_p = predQ(Q,S,N);
+    R_p =  kron(eye(Nu), R);
+    Dy_p = kron(ones(N,1), Dy(:,k));
+    H = B_p'*Q_p*B_p + R_p;
+    f = B_p'*Q_p*(A_p*xtilde(:,k) + Bd_p*Dy_p - yref_p);
+
+    % Output constraint
+    ymax =  [inf; inf; inf; inf; 2*pi/5; inf]*1;
+    ymin = -[inf; inf; inf; inf; 2*pi/5; inf]*1;
+    Ymax = kron(ones(N,1),ymax);
+    Ymin = kron(ones(N,1),ymin);
+    Aqp = [B_p; -B_p];
+    bqp = [Ymax-(A_p*xtilde(:,k)-Bd_p*Dy_p); -Ymin+(A_p*xtilde(:,k)-Bd_p*Dy_p)];
+
+    % Solve quadratic program
+    options = optimset('Display', 'off');
+    H=(H+H')/2; % To impose symmetry in the Hessian matrix
+    V = quadprog(H, f, Aqp,bqp,[],[],[],[],[],options);
+    Dtau(:,k) = V(1:nu);
+    tau(:,k) = Dtau(:,k) + tau_;
+
+    % Apply input forces to the AUV dynamics and add white noise
+    x_next  = awgn(solver_RK([y(:,k);x(:,k)],Ts,tau(:,k),nu_c(:,k),tau_wave),80);
+
+    % Extract the position and velocity vectors
+    x(:,k+1) = x_next(7:12) ; %Ak*(x(:,k)-nu_c) + Bk*tau(:,k) + dnk;
+    y(:,k+1) = x_next(1:6);
+    Dy(:,k+1) = y(:,k+1) - y(:,k);
+    Dx(:,k+1) = x(:,k+1) - x(:,k);
+    xtilde(:,k+1) = [Dx(:,k+1); y(:,k+1)];
+
+    % Updates the previous control input, position and velocities
+    tau_ = tau(:,k);
+
+    %sprintf('Iteration number = %d of %d',k, NoS)
+end
+
+xi = [y;x];
+
+yi = y;
+
+ui = tau;
+
+end
+
+
+
+
